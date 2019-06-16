0323ec880d46c91d3f391c3e47e5820c82a36aa6
Waleed Gamal <Waleed.gamal1000@gmail.com>
2019-06-16 17:54:48 +0200

    fixed eslint errors

--- client/src/containers/App/AppContainer.js
+++ client/src/containers/App/AppContainer.js
@@ -1,4 +1,35 @@
+// @flow
+
 import React from 'react';
+import { connect } from 'react-redux';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-export default () => <div>yay!</div>;
+import Home from '../../components/Home';
+
+type Props = void;
+type State = void;
+class App extends React.Component<Props, State> {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={() => <Home />} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+function mapStateToProps(state: any) {
+  return {};
+}
+
+function mapDispatchToProps(dispatch: (() => any) => any) {
+  return {};
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
